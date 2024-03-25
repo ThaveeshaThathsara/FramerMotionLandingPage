cb5bbb0b0ef7ca5f18f748103032d1fab73461ef
Thaveesha Thathsara <151990674+ThaveeshaThathsara@users.noreply.github.com>
2024-03-25 22:58:36 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // Add other file paths if needed
+  ],
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
